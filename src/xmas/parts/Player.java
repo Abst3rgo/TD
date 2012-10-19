8456da023259cc86aadb885c82cea8b2faed5a6c
chknetsc <chknetsc@lenny.in.fhkn.de>
2012-10-19 12:17:04 +0200

    Start

--- src/xmas/parts/Player.java
+++ src/xmas/parts/Player.java
@@ -1,5 +1,37 @@
 package xmas.parts;
 
 public class Player {
+	
+	private String name;
+	private int gift;
+	
+	public Player(String name, int gift) {
+		this.name = name;
+		this.gift = gift;
+	}
+	
+	public String getname() {
+		return name;
+	}
+	
+	public void setname(String name) {
+		this.name = name;
+	}
 
+	public void setgift(int gift) {
+		this.gift = gift;
+	}
+	
+	public int checkgifts() {
+		return this.gift;
+	}
+	
+	public int gameover() {
+		if ( gift == 0) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
 }
--- test/xmas/parts/PlayerTest.java
+++ test/xmas/parts/PlayerTest.java
@@ -1,5 +1,30 @@
 package xmas.parts;
+import java.io.*;
+import junit.framework.TestCase;
 
-public class PlayerTest {
+public class PlayerTest extends TestCase {
+	Player player;
+	
+	
+	public void setUp() {
+		player = new Player("Horst", 3);
+	}
+	
+	
+	public void testGetValue() {
+		// Life Test
+		player.setgift(2);
+		assertEquals(2, player.checkgifts());
+		// Name Test
+		player.setname("Bernd");
+		assertEquals("Bernd", player.getname());
+		// GameOver Test false
+		assertEquals(1, player.gameover());
+		player.setgift(0);
+		assertEquals(0, player.gameover());
+		
+		
+	}
 
 }
+
